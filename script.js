5d26d30b9f12459d94e3aefcce15cb750d98ae55
rabbanirizwan <rabbani_riwan@yahoo.com>
2019-04-18 14:44:12 +0500

    second

---
+++ script.js
@@ -0,0 +1,123 @@
+function food1() {
+     console.log("chla1");
+      document.querySelector(".second-nav #one a").style.color="orange";
+      document.querySelector(".second-nav #one").style.borderBottom = "1px solid orange";
+       document.querySelector(".second-nav #two a").style.color="black";
+       document.querySelector(".second-nav #three a").style.color="black";
+       document.querySelector(".second-nav #four a").style.color="black";
+       document.querySelector(".second-nav #five a").style.color="black";
+       document.querySelector(".second-nav #six a").style.color="black";
+    
+       document.querySelector(".second-nav #two").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #three").style.borderBottom = "1px solid black";
+    document.querySelector(".second-nav #four").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #five").style.borderBottom = "1px solid black";
+  
+    document.querySelector(".second-nav #six").style.borderBottom = "1px solid black";
+    
+}
+
+function food2() {
+    console.log("chla2");
+     document.querySelector(".second-nav #one a").style.color="black";
+    document.querySelector(".second-nav #two a").style.color="orange";
+    document.querySelector(".second-nav #two").style.borderBottom = "1px solid orange";
+    document.querySelector(".second-nav #one").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #three").style.borderBottom = "1px solid black";
+    document.querySelector(".second-nav #four").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #five").style.borderBottom = "1px solid black";
+  
+    document.querySelector(".second-nav #six").style.borderBottom = "1px solid black";
+     
+    
+    document.querySelector(".second-nav #three a").style.color="black";
+     document.querySelector(".second-nav #four a").style.color="black";
+     document.querySelector(".second-nav #five a").style.color="black";
+     document.querySelector(".second-nav #six a").style.color="black";
+}
+
+function food3() {
+    console.log("chla3");
+    document.querySelector(".second-nav #one a").style.color="black";
+    document.querySelector(".second-nav #two a").style.color="black";
+    document.querySelector(".second-nav #three a").style.color="orange";
+
+    document.querySelector(".second-nav #three").style.borderBottom = "1px solid orange";
+    document.querySelector(".second-nav #two").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #one").style.borderBottom = "1px solid black";
+    document.querySelector(".second-nav #four").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #five").style.borderBottom = "1px solid black";
+  
+    document.querySelector(".second-nav #six").style.borderBottom = "1px solid black";
+    
+
+    document.querySelector(".second-nav #four a").style.color="black";
+    document.querySelector(".second-nav #five a").style.color="black";
+    document.querySelector(".second-nav #six a").style.color="black";    
+}
+function food4() {
+    console.log("chla4");
+    document.querySelector(".second-nav #one a").style.color="black";
+    document.querySelector(".second-nav #two a").style.color="black";
+    document.querySelector(".second-nav #three a").style.color="black";
+    document.querySelector(".second-nav #four a").style.color="orange";
+    document.querySelector(".second-nav #four").style.borderBottom = "1px solid orange";
+    document.querySelector(".second-nav #five a").style.color="black";
+    document.querySelector(".second-nav #six a").style.color="black";
+    document.querySelector(".second-nav #one").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #two").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #three").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #five").style.borderBottom = "1px solid black";
+  
+    document.querySelector(".second-nav #six").style.borderBottom = "1px solid black";
+    
+}
+function food5() {
+    console.log("chla5");
+    document.querySelector(".second-nav #one").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #two").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #three").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #four").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #one a").style.color="black";
+    document.querySelector(".second-nav #two a").style.color="black";
+    document.querySelector(".second-nav #three a").style.color="black";
+    document.querySelector(".second-nav #four a").style.color="black";
+    document.querySelector(".second-nav #five a").style.color="orange";
+    document.querySelector(".second-nav #five").style.borderBottom = "1px solid orange";
+    document.querySelector(".second-nav #six").style.borderBottom = "1px solid black";
+    document.querySelector(".second-nav #six a").style.color="black";
+   
+}
+
+function food6() {
+    console.log("chla6");
+    document.querySelector(".second-nav #one a").style.color="black";
+    document.querySelector(".second-nav #two a").style.color="black";
+    document.querySelector(".second-nav #three a").style.color="black";
+    document.querySelector(".second-nav #four a").style.color="black";
+    document.querySelector(".second-nav #five a").style.color="black";
+    document.querySelector(".second-nav #six a").style.color="orange";
+    document.querySelector(".second-nav #six").style.borderBottom = "1px solid orange";
+    document.querySelector(".second-nav #two").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #three").style.borderBottom = "1px solid black";
+    document.querySelector(".second-nav #four").style.borderBottom = "1px solid black";
+    
+    document.querySelector(".second-nav #five").style.borderBottom = "1px solid black";
+  
+    document.querySelector(".second-nav #one").style.borderBottom = "1px solid black";
+       
+}
